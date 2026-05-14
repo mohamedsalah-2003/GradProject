bda7fa700ffe1596a213c1c482936b8db6e1262c
88nada <nadamohamedafez@gmail.com>
2026-05-14 23:46:41 +0300

    Add/Delete/GetDetails (Device)

--- services/devices.service.js
+++ services/devices.service.js
@@ -7,3 +7,23 @@ export const getAllDevices = async (body) => {
   return response.data;
 };
 
+export const getDevice = async (deviceId) => {
+  const response = await api.get(`/devices/getDevice/${deviceId}`);
+  return response.data;
+};
+
+export const addDevice = async (deviceData) => {
+  const response = await api.post("/devices/createDevice", deviceData);
+  return response.data;
+};
+
+export const updateDevice = async (deviceId, deviceData) => {
+  const response = await api.put(`/devices/updateDevice/${deviceId}`, deviceData);
+  return response.data;
+};
+
+export const deleteDevice = async (deviceId) => {
+  const response = await api.delete(`/devices/deleteDevice/${deviceId}`);
+  return response.data;
+};
+
